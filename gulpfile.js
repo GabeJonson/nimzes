316cd7c09e1690527fe3cd031a60d98dc13df3de
gabe <sky3107@gmail.com>
2016-06-30 20:05:38 +0300

    add some new files
    add pug instead jade
    
    modefide files
    
    rename app to src
    
    add sass dlobal import
    
    Update gulpfile.js
    
    Add worked fonts
    
    Update package.json
    
    Update packege.json
    
    Update package.json
    
    Add "gulp-cssmin": "^0.1.7",
    
    Update gulpfile.js
    
    Add cssmin pipe
    
    Update gulpfile.js
    
    Forget 2 add
        cssmin = require('gulp-cssmin'), // minify css
    
    Start project fast
    
    delete some files
    
    remove css and html dev folders
    
    remove logo from img
    
    Update package.json
    
    Update package.json
    
    Update _global.scss
    
    Update _varibles.scss
    
    Update _varibles.pug
    
    Update _varibles.scss
    
    Update index.pug
    
    Update _mixins.scss
    
    Update _mixins.pug
    
    Update _global.scss
    
    Create .bowerrc
    
    Update .gitignore
    
    Update package.json
    
    Update bower.json
    
    Update _vendor.scss
    
    some restructure project
    
    Update package.json
    
    rework project
    
    add vertical grid generate
    
    add move fonts
    
    add helpers and grid
    
    rework
    
    Update main.scss
    
    Update _functions.scss
    
    Update _settings.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,138 @@
-var gulp = require('gulp'), // gulp
-	csso = require('gulp-csso'), //// Минификация CSS
-	imagemin = require('gulp-imagemin'), // Минификация изображений
-	concat = require('gulp-concat'), // Склейка файлов
-	uglify = require('gulp-uglify'), // Минификация JS
-	sass = require('gulp-sass'), // Sass
-	autoprefixer = require('gulp-autoprefixer'),
-	htmlhint = require("gulp-htmlhint"),
-	livereload = require('gulp-livereload'),
-	connect = require('gulp-connect'),
-	jade = require('gulp-jade'), // jade
-	sourcemaps = require('gulp-sourcemaps'); // Reload
+var gulp         = require('gulp'), // gulp
+    csso         = require('gulp-csso'), //// Минификация CSS
+    concat       = require('gulp-concat'), // Склейка файлов
+    uglify       = require('gulp-uglify'), // Минификация JS
+    sass         = require('gulp-sass'), // Sass
+    autoprefixer = require('gulp-autoprefixer'),
+    cssmin       = require('gulp-cssmin'), // minify css
+    livereload   = require('gulp-livereload'),
+    connect      = require('gulp-connect'),
+    pug          = require('gulp-pug'), // pug
+    sourcemaps   = require('gulp-sourcemaps'); // Reload
 
 var path = {
-	build: {
-		js: 'build/js',
-		css: 'build/css',
-		img: 'build/img',
-		html: 'build/'
-	},
-	src: {
-		js: 'app/js/*.js',
-		scss: 'app/sass/**/*.scss',
-		css: 'app/css',
-		main: 'app/css/*',
-		img: 'app/img/*',
-		html: 'app/*.html',
-		jade: 'app/jade/*.jade',
-		jadeWatch: 'app/jade/**/*.jade',
-		jadeDest: 'app/'
-	}
-}
+    build: {
+        js:    'build/js',
+        css:   'build/css',
+        img:   'build/img',
+        html:  'build/',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        js:      'src/js/*.js',
+        scss:    'src/sass/**/*.scss',
+        css:     'src/css',
+        main:    'src/css/*',
+        img:     'src/img/*',
+        html:    'src/html/*.html',
+        pug:     'src/pug/**/*.pug',
+        pugDest: 'src/html/',
+        fonts:   'src/fonts/*'
+    }
+};
 
 gulp.task('js:build', function () {
-		gulp.src(path.src.js)
-			.pipe(uglify())
-			.pipe(gulp.dest(path.build.js));
+    gulp.src(path.src.js)
+        .pipe(uglify())
+        .pipe(gulp.dest(path.build.js));
 });
 
 gulp.task('js', function () {
-		gulp.src(path.src.js)
-		.pipe(connect.reload());
+    gulp.src(path.src.js)
+        .pipe(connect.reload());
 });
 
 gulp.task('sass', function () {
-	gulp.src(path.src.scss)
-		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 5 versions'],
-			cascade: false
-		}))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(path.src.css))
-		.pipe(connect.reload());
+    gulp.src(path.src.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.src.css))
+        .pipe(connect.reload());
 });
 
 gulp.task('csso:build', function () {
-	return gulp.src(path.src.main)
-		.pipe(gulp.dest(path.build.css));
+    return gulp.src(path.src.main)
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.build.css));
 });
 
 gulp.task('img:build', function() {
-	gulp.src(path.src.img)
-		.pipe(imagemin())
-		.pipe(gulp.dest(path.build.img));
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.build.img));
 });
 
-gulp.task('html:build', function () {
-	gulp.src(path.src.html)
-		.pipe(gulp.dest(path.build.html));
+gulp.task('html:build', ['pug:build'], function () {
+    gulp.src(path.src.html)
+        .pipe(gulp.dest(path.build.html));
 });
 
-gulp.task('html', function () {
-	gulp.src(path.src.html)
-		.pipe(connect.reload());
+gulp.task('html', ['pug'], function () {
+    gulp.src(path.src.html)
+        .pipe(connect.reload());
 });
 
-gulp.task('jade', function() {
-	return gulp.src(path.src.jade)
-		.pipe(jade({pretty: true}))
-		.pipe(gulp.dest(path.src.jadeDest))
+gulp.task('pug', function() {
+    return gulp.src(path.src.pug)
+        .pipe(pug({
+            pretty: true,
+            locals: {
+                cssPath:      '../css/',
+                jsPath:       '../js/',
+                imgPath:      '../img/',
+                cssName:      'main',
+                projectTitle: 'title'
+            }
+        }))
+        .pipe(gulp.dest(path.src.pugDest))
+});
+
+gulp.task('pug:build', function() {
+    return gulp.src(path.src.pug)
+        .pipe(pug({
+            pretty: true,
+            locals: {
+                cssPath:      'css/',
+                jsPath:       'js/',
+                imgPath:      'img/',
+                cssName:      'main',
+                projectTitle: 'title'
+            }
+        }))
+        .pipe(gulp.dest(path.src.pugDest))
 });
 
 gulp.task('fonts:build', function() {
-	return gulp.src('./app/fonts/*')
-		.pipe(gulp.dest('./build/fonts/'))
+    return gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
 });
 
 gulp.task('connect', function() {
-	connect.server({
-		root: 'app',
-		port: 9000,
-		livereload: true
-	});
+    connect.server({
+        root: 'src',
+        port: 5000,
+        livereload: true
+    });
 });
 
 gulp.task('watch', function () {
-	gulp.watch(path.src.scss, ['sass']);
-	gulp.watch(path.src.js, ['js']);
-	gulp.watch(path.src.jadeWatch, ['jade']);
-	gulp.watch(path.src.html, ['html']);
+    gulp.watch(path.src.scss, ['sass']);
+    gulp.watch(path.src.js, ['js']);
+    gulp.watch(path.src.pug, ['pug']);
+    gulp.watch(path.src.html, ['html']);
 });
 
-gulp.task('default', ['connect', 'html', 'jade', 'sass', 'js', 'watch']);
+gulp.task('default', ['connect', 'html', 'sass', 'js', 'watch']);
 
 // Сборка проекта
 gulp.task('b', function() {
-	gulp.run('html:build');
-	gulp.run('csso:build');
-	gulp.run('js:build');
-	gulp.run('img:build');
-	gulp.run('fonts:build');
+    gulp.run('html:build');
+    gulp.run('csso:build');
+    gulp.run('js:build');
+    gulp.run('img:build');
+    gulp.run('fonts:build');
 });
